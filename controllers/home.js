f799c0b808d70208d32b44d3c1704cfc6ffade92
Tejas <tejasmishra2k18@gmail.com>
2020-11-17 22:46:51 +0530

    display all the tasks in database on the home page

--- controllers/home.js
+++ controllers/home.js
@@ -5,7 +5,20 @@ its export in used in index.js router file
 ***********************************************************
 ************************************************************/
 
+// requiring the model bcz, it is required here to display tasks in the database
+const ToDoList = require('../models/todolist.js');
+
 
 module.exports.home = function(req, res) {
-    return res.render('index');
+
+    // fetching all the tasks in database
+    ToDoList.find({}, function(err, taskstodo) {
+        if (err) {
+            console.log('error in fetching the database');
+            return;
+        }
+        return res.render('index', {
+            tasks: taskstodo // initialising the locals
+        })
+    })
 };
