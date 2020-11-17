7918c84aa6901db72357094fc51651ce1dfd7787
Tejas <tejasmishra2k18@gmail.com>
2020-11-17 22:22:02 +0530

    add task to database , change in add.js controller

--- controllers/add.js
+++ controllers/add.js
@@ -5,7 +5,20 @@ its export in used in add.js router file
 ***********************************************************
 ************************************************************/
 
+// requiring the model bcz, it is required here to add task to the database
+const ToDoList = require('../models/todolist.js');
+
 module.exports.add = function(req, res) {
     console.log(req.query);
-    return res.end('<h1> u reached this page because u added a task</h1>');
+
+    // adding the task to database
+    ToDoList.create(req.query, function(err, newTask) {
+        if (err) {
+            console.log('Error in adding the task');
+            return;
+        }
+        console.log('new added task is ', newTask)
+        return res.redirect('back');
+    })
+
 };
--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express(); // now app contain all the functionalities of express
 const port = 8000; // defining the port
 
 const db = require('./config/mongoose'); // requiring the config b/w mongo and mongodb
-const ToDoList = require('./models/todolist.js'); // requiring the model 
+
+/* // const ToDoList = require('../models/todolist.js'); // requiring the model 
+// commented the above line , bcz I wont require that database here 
+// I will need that in controller, so these models are required there */
 
 //setting up the view engine
 app.set('view engine', 'ejs'); // defining the view engine
@@ -16,12 +19,6 @@ app.use(express.static('assets')); // middleware for static files
 app.use(express.urlencoded()); //  middleware
 
 app.use('/', require('./routes/index')); // defining the router
-// app.get('/', function(req, res) {
-//     return res.render('index');
-// });
-// app.get('/add', function(req, res) {
-//     console.log(req.query);
-// })
 
 // listening to the port 8000
 app.listen(port, function(err) {
